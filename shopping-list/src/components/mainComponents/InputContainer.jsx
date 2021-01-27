62c6dc8c72780989c93f554dadbe83839235b554
DanBourdier <danbourdier@aol.com>
2021-01-27 13:24:36 -0500

    added basecode for InputContainer

---
+++ shopping-list/src/components/mainComponents/InputContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const InputContainer = props => {
+
+
+  return (
+    <section>
+      Input Container!!!
+    </section>
+  )
+
+}
+
+
+export default InputContainer

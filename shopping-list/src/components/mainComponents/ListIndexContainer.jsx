2ec5da8fada02a4ab9fafbcc6533b84b851aaa28
DanBourdier <danbourdier@aol.com>
2021-01-27 13:25:19 -0500

    added basecode for ListIndexContainer

--- shopping-list/src/components/mainComponents/ListIndexContainer.jsx
+++ shopping-list/src/components/mainComponents/ListIndexContainer.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
 
+
+const ListIndexContainer = props => {
+
+
+  return (
+    <section>
+      List Index Container!!!
+    </section>
+  )
+
+}
+
+
+export default ListIndexContainer

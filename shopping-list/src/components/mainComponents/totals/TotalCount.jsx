2fd87aec518a2aff77e0db1e95c3331c4b088fc9
DanBourdier <danbourdier@aol.com>
2021-01-27 13:54:48 -0500

    created two components for totals folder

---
+++ shopping-list/src/components/mainComponents/totals/TotalCount.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const TotalCount = props => {
+
+
+  return (
+    <section>
+      Total Count
+    </section>
+  )
+
+}
+
+
+export default TotalCount
---
+++ shopping-list/src/components/mainComponents/totals/TotalPrice.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const TotalPrice = props => {
+
+
+  return (
+    <section>
+      Total Price
+    </section>
+  )
+
+}
+
+
+export default TotalPrice

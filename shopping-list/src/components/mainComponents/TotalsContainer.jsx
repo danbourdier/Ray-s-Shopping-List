9bdf7ec7867efcc04f3fde3c3b54ddb92839680f
DanBourdier <danbourdier@aol.com>
2021-01-27 13:25:53 -0500

    added basecode for Totals Container

---
+++ shopping-list/src/components/mainComponents/TotalsContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const TotalsContainer = props => {
+
+
+  return (
+    <section>
+      Totals Container!!!
+    </section>
+  )
+
+}
+
+
+export default TotalsContainer

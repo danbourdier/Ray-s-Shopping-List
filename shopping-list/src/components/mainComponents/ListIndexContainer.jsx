c55c834cfe0179aaf97fe9755dd22c8ee63e18f2
DanBourdier <danbourdier@aol.com>
2021-01-27 13:40:07 -0500

    added structuring for ListIndexContainer

--- shopping-list/src/components/mainComponents/ListIndexContainer.jsx
+++ shopping-list/src/components/mainComponents/ListIndexContainer.jsx
@@ -7,6 +7,13 @@ const ListIndexContainer = props => {
   return (
     <section>
       List Index Container!!!
+      <div>
+        Pending List
+      </div>
+
+      <div>
+        Crossed List
+      </div>
     </section>
   )
 

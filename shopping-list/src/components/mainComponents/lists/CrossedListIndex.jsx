921c2a463336314658a2ef9d27c5e1a4ac281f29
DanBourdier <danbourdier@aol.com>
2021-01-27 13:43:38 -0500

    added basecode for pending and crossed lists

---
+++ shopping-list/src/components/mainComponents/lists/CrossedListIndex.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const CrossedListIndex = props => {
+
+
+  return (
+    <section>
+      Crossed List Index!
+    </section>
+  )
+
+}
+
+
+export default CrossedListIndex
---
+++ shopping-list/src/components/mainComponents/lists/PendingListIndex.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const PendingListIndex = props => {
+
+
+  return (
+    <section>
+      Pending List Index!
+    </section>
+  )
+
+}
+
+
+export default PendingListIndex
